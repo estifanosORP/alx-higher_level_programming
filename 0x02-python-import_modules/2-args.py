b8ee00c5d98086d788bb22ff601355d6fabe4a84
EstifanosORP <estifanos9a@gmail.com>
2022-06-07 23:22:58 +0900

    print arguments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,10 +2,12 @@
 
 if __name__ == "__main__":
     import sys
-    tot = len(sys.argv)
-    if (tot == 2):
-        print("{} argument:".format(tot-1))
+    tot = len(sys.argv) - 1
+    if (tot == 0):
+        print("0 arguments.")
+    elif (tot == 1):
+        print("1 argument:")
     else:
-        print("{} arguments:".format(tot-1))
-    for x in range(tot-1):
+        print("{} arguments:".format(tot))
+    for x in range(tot):
         print("{}: {}".format(x+1, sys.argv[x+1]))
