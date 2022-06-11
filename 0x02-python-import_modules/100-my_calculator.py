9b403889a1b4bff3f92485257da34042b48cf974
EstifanosORP <estifanos9a@gmail.com>
2022-06-12 00:56:20 +0900

    corrected formatting

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -20,10 +20,10 @@ if __name__ == "__main__":
     b = int(argv[3])
 
     if op == '+':
-        print("{} {} {} == {}".format(a, op, b, add(a, b)))
+        print("{} {} {} = {}".format(a, op, b, add(a, b)))
     elif op == '-':
-        print("{} {} {} == {}".format(a, op, b, sub(a, b)))
+        print("{} {} {} = {}".format(a, op, b, sub(a, b)))
     elif op == '*':
-        print("{} {} {} == {}".format(a, op, b, mul(a, b)))
+        print("{} {} {} = {}".format(a, op, b, mul(a, b)))
     elif op == '/':
-        print("{} {} {} == {}".format(a, op, b, div(a, b)))
+        print("{} {} {} = {}".format(a, op, b, div(a, b)))
