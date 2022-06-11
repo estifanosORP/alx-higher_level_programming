d0d28f5c7cecc32e550dbfbf77ad15d25b0c0c9a
EstifanosORP <estifanos9a@gmail.com>
2022-06-12 00:33:41 +0900

    running only as script

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
-import hidden_4
-lst = dir(hidden_4)
 
-for module in lst:
-    if module[:2] != '__':
-        print(module)
+if __name__ == '__main__':
+    import hidden_4
+    lst = dir(hidden_4)
+
+    for module in lst:
+        if module[:2] != '__':
+            print(module)
