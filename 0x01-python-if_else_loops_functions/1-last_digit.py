12208619ab0d0a2f822debb5e8394571ee6e46d8
EstifanosORP <estifanos9a@gmail.com>
2022-06-15 21:44:03 +0900

    adjustment for pycodestyle linter

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -4,10 +4,10 @@ if __name__ == "__main__":
     import random
     number = random.randint(-10000, 10000)
 
-    last_digit = abs(number) % 10
-    if last_digit > 5:
-        print(f"Last digit of {number} is {last_digit} and is greater than 5")
-    elif last_digit < 6 and last_digit > 0:
-        print(f"Last digit of {number} is {last_digit} and is less than 6 and not 0")
-    elif last_digit == 0:
-        print(f"Last digit of {number} is {last_digit} and is 0")
+    lst = abs(number) % 10
+    if lst > 5:
+        print(f"Last digit of {number} is {lst} and is greater than 5")
+    elif lst < 6 and lst > 0:
+        print(f"Last digit of {number} is {lst} and is less than 6 and not 0")
+    elif lst == 0:
+        print(f"Last digit of {number} is {lst} and is 0")
