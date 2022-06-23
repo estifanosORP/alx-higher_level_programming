fc43241c77f2ef721b39cd3ae9248ddb8f15ba8b
estifanosORP <estifanos9a@gmail.com>
2022-06-23 23:00:31 +0900

    update on square class

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3.py
+
+"""
+module that defines different objects
+"""
+
+
+class Square:
+    """
+    A class object for square
+    """
+
+    def __init__(self, size=0):
+        if type(size) == int and size >= 0:
+            self.__size = size
+        else:
+            if type(size) != int:
+                raise TypeError("size must be an integer")
+            elif size < 0:
+                raise ValueError("size must be >= 0")
+
+    def area(self):
+        """
+        calculates the area of the square
+        """
+        return self.__size**2
