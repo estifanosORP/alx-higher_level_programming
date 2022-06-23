8969abc78d0247460b4fea8e60e005dbcaae63b6
estifanosORP <estifanos9a@gmail.com>
2022-06-23 22:10:10 +0900

    prive attribute

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/1-square.py
+
+"""
+module that defines different objects
+"""
+
+
+class Square:
+    """
+    A class object for square
+    """
+    
+    def __init__(self, size):
+        self.__size = size
