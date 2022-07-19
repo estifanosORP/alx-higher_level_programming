a3304153b9ee46ec6fe7f7cdd2f52d635dfda239
estifanosORP <estifanos9a@gmail.com>
2022-07-19 21:16:41 +0900

    Task #7

---
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -0,0 +1,17 @@
+#!/bin/usr/python3
+"""
+module to multiply to matrices
+"""
+
+import numpy as np
+
+
+def lazy_matrix_mul(m_a, m_b):
+    """
+    function that multiplies two matrices using the numpy library
+    args:
+        m_a: first matrix
+        m_b: second matrix
+    """
+
+    return np.matmul(np.array(m_a), np.array(m_b)).tolist()
