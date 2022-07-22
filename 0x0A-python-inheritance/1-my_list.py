f15828e4e6259d08094b7b8b362d6b7bfa13f2a1
estifanosORP <estifanos9a@gmail.com>
2022-07-22 23:30:06 +0900

    task #1

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+module for a class that inherits from the class object
+"""
+
+
+class MyList(list):
+    """class inherits from the class object"""
+    def print_sorted(self):
+        """
+        prints the list but in ascending order. 
+        The elements of the list are int
+        """
+        lst_cpy = self[:]
+        lst_cpy.sort()
+        print(lst_cpy)
