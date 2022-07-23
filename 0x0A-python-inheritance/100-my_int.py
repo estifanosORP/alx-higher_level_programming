6799a20bf0121246477c704144916077f874087e
estifanosORP <estifanos9a@gmail.com>
2022-07-23 16:57:46 +0900

    task #12

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+module for a custom class that inherits from
+the int class
+"""
+
+
+class MyInt(int):
+    def __int__(self):
+        """initializes the object"""
+        super().__init__()
+
+    def __eq__(self, other):
+        """method for =="""
+        return (int.__ne__(self, other))
+
+    def __ne__(self, other):
+        """method for !="""
+        return (int.__eq__(self, other))
