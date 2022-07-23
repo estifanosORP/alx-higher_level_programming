209879e3a4e436fb9deea600ed9b715b11beb7e3
estifanosORP <estifanos9a@gmail.com>
2022-07-23 17:19:59 +0900

    task #13

---
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+module that manipulates the attributes of a class
+"""
+
+
+def add_attribute(obj, att_name, att_value):
+    """
+    function that adds a new attribute to an object if it's
+    possible
+    args:
+        obj: an instance of an object
+    """
+    try:
+        if att_name not in obj.__dict__.keys():
+            setattr(obj, att_name, att_value)
+    except Exception:
+        raise TypeError("can't add new attribute")
