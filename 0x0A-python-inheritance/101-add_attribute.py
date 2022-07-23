23e25c0f22f6848c5f6cc8ab7c91e05fcbf9abd2
estifanosORP <estifanos9a@gmail.com>
2022-07-23 18:52:18 +0900

    task #13-update

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -11,11 +11,6 @@ def add_attribute(obj, att_name, att_value):
     args:
         obj: an instance of an object
     """
-    if (
-        att_name not in dir(obj) and
-        obj.__class__.__module__ != 'builtins' and
-        '__slots__' not in dir(obj)
-       ):
-        setattr(obj, att_name, att_value)
-    else:
+    if not hasattr(obj, '__dict__'):
         raise TypeError("can't add new attribute")
+    setattr(obj, att_name, att_value)
